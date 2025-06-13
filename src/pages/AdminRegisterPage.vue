03ee23ea76a730f92accd85565f6d30d6ca1ffe5
ayemfia <masofia.beron@gmail.com>
2025-06-14 03:48:16 +0800

    added admin regis and change in css

--- src/pages/AdminRegisterPage.vue
+++ src/pages/AdminRegisterPage.vue
@@ -1,68 +1,112 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-form @submit.prevent="registerAdmin">
-      <p>First Name</p>
-      <q-input
-        filled
-        v-model="form.first_name"
-        lazy-rules
-        :rules="[(val) => !!val || 'Please enter your first name.']"
-      />
-      <p>Last Name</p>
-      <q-input
-        filled
-        v-model="form.last_name"
-        lazy-rules
-        :rules="[(val) => !!val || 'Please enter your last name.']"
-      />
-      <p>Email</p>
-      <q-input
-        filled
-        v-model="form.email"
-        type="email"
-        lazy-rules
-        :rules="[
-          (val) => !!val || 'Please enter your email.',
-          (val) => val.includes('@iskolarngbayan.pup.edu.ph') || 'Please use your PUP email only.',
-        ]"
-      />
-      <p>Contact Number</p>
-      <q-input
-        filled
-        v-model="form.contact"
-        lazy-rules
-        :rules="[(val) => !!val || 'Please enter your contact number.']"
-      />
+  <div class="q-pa-md form-container">
+    <div class="column items-center q-mb-md">
+      <label class="form-title">SIGN UP</label>
+      <label class="subtitle">Let's Get You Set Up</label>
+    </div>
+    <div class="column q-gutter-sm">
+      <q-form @submit.prevent="registerAdmin">
+        <label class="names">First Name</label>
+        <q-input
+          filled
+          dense
+          v-model="form.first_name"
+          lazy-rules
+          :rules="[(val) => !!val || 'Please enter your first name.']"
+          class="text-box"
+        />
+        <label class="names">Last Name</label>
+        <q-input
+          filled
+          dense
+          v-model="form.last_name"
+          lazy-rules
+          :rules="[(val) => !!val || 'Please enter your last name.']"
+          class="text-box"
+        />
+        <div class="row">
+          <div class="col q-gutter-sm">
+            <label class="names">Email</label>
+            <q-input
+              filled
+              dense
+              v-model="form.email"
+              type="email"
+              lazy-rules
+              :rules="[
+                (val) => !!val || 'Please enter your email.',
+                (val) =>
+                  val.includes('@iskolarngbayan.pup.edu.ph') || 'Please use your PUP email only.',
+              ]"
+              class="text-box-2 q-mr-lg"
+            />
+          </div>
+          <div class="col q-gutter-sm">
+            <label class="names">Contact Number</label>
+            <q-input
+              filled
+              dense
+              v-model="form.contact"
+              lazy-rules
+              :rules="[(val) => !!val || 'Please enter your contact number.']"
+              class="text-box-2"
+            />
+          </div>
+        </div>
 
-      <p>Password</p>
-      <q-input
-        filled
-        v-model="form.password"
-        type="password"
-        :hint="passwordStrength"
-        :color="passwordStrengthColor"
-        lazy-rules
-        :rules="[
-          (val) => !!val || 'Please enter your password.',
-          (val) => val.length >= 8 || 'Must be at least 8 characters long.',
-          (val) =>
-            (/[A-Z]/.test(val) && /[0-9]/.test(val) && /[^a-zA-Z0-9]/.test(val)) ||
-            'Must contain an uppercase letter, a number, and a special character.',
-        ]"
-      />
-      <p>Confirm Password</p>
-      <q-input
-        v-model="form.confirmPassword"
-        type="password"
-        lazy-rules
-        :rules="[
-          (val) => !!val || 'Please confirm your password.',
-          (val) => val === form.password || 'Passwords do not match.',
-        ]"
-      />
-      <q-btn label="Register" @click="registerAdmin" color="primary" />
-    </q-form>
-  </q-page>
+        <label class="names">Password</label>
+        <q-input
+          filled
+          dense
+          v-model="form.password"
+          type="password"
+          :hint="passwordStrength"
+          :color="passwordStrengthColor"
+          lazy-rules
+          :rules="[
+            (val) => !!val || 'Please enter your password.',
+            (val) => val.length >= 8 || 'Must be at least 8 characters long.',
+            (val) =>
+              (/[A-Z]/.test(val) && /[0-9]/.test(val) && /[^a-zA-Z0-9]/.test(val)) ||
+              'Must contain an uppercase letter, a number, and a special character.',
+          ]"
+          class="text-box"
+        />
+        <label class="names">Confirm Password</label>
+        <q-input
+          filled
+          dense
+          v-model="form.confirmPassword"
+          type="password"
+          lazy-rules
+          :rules="[
+            (val) => !!val || 'Please confirm your password.',
+            (val) => val === form.password || 'Passwords do not match.',
+          ]"
+          class="text-box"
+        />
+        <div class="column items-center q-pt-xs">
+          <q-btn
+            class="sign-up"
+            push
+            color="primary"
+            text-color="white"
+            label="SIGN UP"
+            @click="registerAdmin"
+          />
+        </div>
+
+        <div class="column items-center q-mb-xs">
+          <label class="already">
+            Already have an account?
+            <router-link to="/admin/login" name="admin-login" class="signup-login-link"
+              >Log In</router-link
+            >
+          </label>
+        </div>
+      </q-form>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -131,7 +175,7 @@ async function registerAdmin() {
       alert(data.error || 'Registration failed.')
     } else {
       alert('Registration successful!')
-      router.push('/admin-login')
+      router.push('/admin/login')
     }
   } catch (error) {
     alert('An error occurred.')
