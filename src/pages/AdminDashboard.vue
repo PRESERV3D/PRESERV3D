d9ffe1fea78c052c757869f11813da53b77a3e89
ayemfia <masofia.beron@gmail.com>
2025-06-16 22:34:01 +0800

    admin box-1 changes

--- src/pages/AdminDashboard.vue
+++ src/pages/AdminDashboard.vue
@@ -1,16 +1,27 @@
 <template>
   <q-page class="q-pa-md">
     <div class="row">
-      <div class="q-mt-md box-1">
-        <p class="q-ml-lg admin-title">Explore & Manage Cultural <br />Heritage Assets</p>
-        <p class="q-ml-lg admin-subtitle">
-          Access digital artifacts, document, and research <br />tools — all in one place.
-        </p>
-        <div class="q-ml-xs q-gutter-md">
-          <q-btn to="/artifacts" label="Explore Artifacts" class="btn-explore" />
-          <q-btn to="/documents" label="Browse Documents" class="btn-document" />
+      <div class="q-mt-md box-1 row items-center">
+        <div class="col-7 q-gutter-xs">
+          <p class="q-ml-lg admin-title">Explore & Manage Cultural Heritage Assets</p>
+          <p class="q-ml-lg admin-subtitle">
+            Access digital artifacts, document, and research <br />
+            tools — all in one place.
+          </p>
+          <div class="q-ml-xs q-gutter-md">
+            <q-btn to="/artifacts" label="Explore Artifacts" class="btn-explore" />
+            <q-btn to="/documents" label="Browse Documents" class="btn-document" />
+          </div>
+        </div>
+        <div class="col-5 q-gutter-xs">
+          <q-img
+            src="src/assets/img/trophy-document.png"
+            alt="Trophy and Document"
+            class="trophies"
+          />
         </div>
       </div>
+
       <div class="q-mt-md box-2">
         <p class="q-ml-lg admin-title-2">Upload</p>
       </div>
@@ -27,6 +38,7 @@
           <q-btn to="/documents" label="Browse Documents" class="btn-document" />
         </div>
       </div>
+
       <div class="box-4">
         <p class="q-ml-lg admin-title-2">Recently Uploaded</p>
       </div>
