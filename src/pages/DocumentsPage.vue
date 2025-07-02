75d90e41eddb419b7d5b3ab1fe5115845f13bac2
ayemfia <masofia.beron@gmail.com>
2025-07-02 14:34:41 +0800

    changed /artifacts to /uploads in DocumentsPage.vue

--- src/pages/DocumentsPage.vue
+++ src/pages/DocumentsPage.vue
@@ -3,8 +3,8 @@
     <div class="page-header">
       <div class="q-mt-xs title">Documents</div>
       <div class="q-mb-md sub-font-3 row items-baseline justify-between">
-        <div>Browse selected digital books from the university archives.</div>
-        <q-btn to="/artifacts" label="Add New" class="btn-add" no-caps />
+        <div class="q-ml-sm">Browse selected digital books from the university archives.</div>
+        <q-btn to="/upload" label="Add New" class="btn-add" no-caps />
       </div>
     </div>
 
