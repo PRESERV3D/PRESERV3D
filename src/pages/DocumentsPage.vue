0c7e7096edb97bc8f44517ede47b328a604e0bee
ayemfia <masofia.beron@gmail.com>
2025-07-04 22:58:46 +0800

    change font subtitle in doc page

--- src/pages/DocumentsPage.vue
+++ src/pages/DocumentsPage.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-md">
     <div class="page-header">
       <div class="q-mt-xs title">Documents</div>
-      <div class="q-mb-md sub-font-3 row items-baseline justify-between">
+      <div class="q-mb-md subtitle row items-baseline justify-between">
         <div class="q-ml-sm">Browse selected digital books from the university archives.</div>
         <q-btn
           v-if="isAdmin"
