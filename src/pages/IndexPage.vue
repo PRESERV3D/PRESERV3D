99c35e9ce5c6c2155705c8892de8a874dbed9e75
llexxie <alexistinamisan23@gmail.com>
2025-07-22 23:02:53 +0800

    see all fix position

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -269,8 +269,8 @@
         </div>
 
         <!-- See All Link -->
-        <div class="row justify-end q-pr-sm q-pb-sm">
-          <router-link to="/collections" class="see-all-link" style="margin-top: 0.5rem">
+        <div class="row justify-end q-pr-sm" style="margin-top: -1.25rem">
+          <router-link to="/collections" class="see-all-link">
             See All
             <q-icon name="arrow_forward" size="16px" class="q-ml-xs" />
           </router-link>
