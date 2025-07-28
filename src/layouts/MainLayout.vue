a67c2a5ee61465229b3ad21aee028c8589e195c2
Joefhanie <joefhanie@gmail.com>
2025-07-29 04:36:56 +0800

    working forgot password and fixed email redirection

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -286,7 +286,7 @@ const handleLogout = async () => {
   try {
     if (confirm('Are you sure you want to logout?')) {
       await userStore.signOut()
-      router.replace('user/login')
+      router.replace('/user/login')
     }
   } catch (error) {
     console.error('Error signing out:', error)
