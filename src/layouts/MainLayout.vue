85c1957b9153be0ae7dd2e7cf970332115718bda
katrinarey <rey.katrina22@gmail.com>
2025-09-09 20:27:09 +0800

    fix userfiltering export

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -241,8 +241,8 @@ import { useQuasar } from 'quasar'
 import { useUserStore } from 'src/stores/user'
 import { useSearchStore } from 'src/stores/searchStore'
 import { useDocumentsStore } from 'src/stores/documentsStore'
-import { useDocumentsFilter } from 'src/utils/useFiltering'
-const { clearFilters } = useDocumentsFilter()
+import { useFiltering } from 'src/utils/useFiltering'
+const { clearFilters } = useFiltering()
 
 const $q = useQuasar()
 const userStore = useUserStore()
