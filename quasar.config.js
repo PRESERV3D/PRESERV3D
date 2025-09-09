abf625b36a0fb80af1e495106882b59898ef3056
Joefhanie <joefhanie@gmail.com>
2025-09-10 06:03:27 +0800

    updated data quality checks and added generate summary w mini game

--- quasar.config.js
+++ quasar.config.js
@@ -97,7 +97,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ['Dialog', 'Loading', 'Notify'],
     },
 
     // animations: 'all', // --- includes all animations
