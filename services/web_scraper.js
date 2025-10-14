3c991c3d0fab72b656ff4d04816638b1c85d665f
Joefhanie <joefhanie@gmail.com>
2025-10-15 02:33:09 +0800

    feat: specify executable path for Puppeteer in web scraper

--- services/web_scraper.js
+++ services/web_scraper.js
@@ -139,6 +139,7 @@ async function scrapeDuckDuckGo(page, query) {
 
     browser = await puppeteer.launch({
       headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
